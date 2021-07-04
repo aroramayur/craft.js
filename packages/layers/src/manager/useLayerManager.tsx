32b65198e978d716e0679d531c3145c5957cc99e
Prev Wong <prevwong@gmail.com>
2021-07-05 01:22:36 +0800

    fix: useLayerManager collector type

--- packages/layers/src/manager/useLayerManager.tsx
+++ packages/layers/src/manager/useLayerManager.tsx
@@ -5,7 +5,6 @@ import { LayerManagerContext } from './context';
 
 import { LayerState } from '../interfaces';
 
-export function useLayerManager<C>(collector?: (state: LayerState) => C);
 export function useLayerManager<C>(collector?: (state: LayerState) => C) {
   const { store } = useContext(LayerManagerContext);
   const collected = useCollector(store, collector);
